daeb0f88376b67b8538a9fee0611f4ce1501bec8
jon <jon.halquist@gmail.com>
2022-05-02 19:33:13 -0500

    attempting to implement audio playback

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import AudioPlayer from 'react-h5-audio-player';
 
 import { getOneSong } from '../../store/song';
 
 import './SongDetail.css';
+import 'react-h5-audio-player/lib/styles.css';
 
 const SongDetail = () => {
   const dispatch = useDispatch();
@@ -19,9 +21,17 @@ const SongDetail = () => {
     return null;
   }
 
+  console.log('url', song.url);
+
   return (
     <div id='mainSongDetailContent'>
       <div id='songDetailBlade'>{song.title}</div>
+      <AudioPlayer
+    autoPlay
+    src='./file_example_MP3_700KB.mp3'
+    onPlay={e => console.log("onPlay")}
+    // other props here
+  />
     </div>
   )
 
