22862d26187c88c03c85bd828bedf0efc7c890bb
jon <jon.halquist@gmail.com>
2022-05-05 18:42:18 -0500

    fixed button styling for song detail page

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
 
-function LoginFormModalButton({ displayText, warning }) {
+function LoginFormModalButton({ displayText, warning, passId }) {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)} className='homeText'>{displayText}</button>
+      <button onClick={() => setShowModal(true)} className='homeText' id={passId || 'none'}>{displayText}</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm warning={warning} />
