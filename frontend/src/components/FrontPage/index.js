956acd2a87f04207e4bc3713345279aa55421df8
jon <jon.halquist@gmail.com>
2022-07-06 14:02:49 -0500

    fixed picks channel from reloading on heart

--- frontend/src/components/FrontPage/index.js
+++ frontend/src/components/FrontPage/index.js
@@ -23,22 +23,6 @@ const FrontPage = ({isLoaded}) => {
     sessionUserId = sessionUser.id
   }
 
-  useEffect(() => {
-    dispatch(getSongs())
-      .then((ret) => {
-        setSongList(ret)
-      })
-      .then(setLoaded(true))
-  },[]);
-
-  useEffect(() => {
-    dispatch(getSongs())
-      .then((ret) => {
-        setSongList(ret)
-      })
-      .then(setLoaded(true))
-  },[hearted]);
-
   // shuffles the songlist to return some random songs
   function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -53,14 +37,42 @@ const FrontPage = ({isLoaded}) => {
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
+      }
+
+      return array;
     }
 
-    return array;
-  }
+    const [randomSongList, setRandomSongList] = useState(shuffle(songList.map(song => song)));
+    // let randomSongList = songList.map(song => song);
 
-  // creates a random songlist to give variety to home page lists
-  let randomSongList = songList.map(song => song);
-  randomSongList = shuffle(randomSongList);
+  useEffect(() => {
+    dispatch(getSongs())
+      .then((ret) => {
+        setSongList(ret)
+        return ret;
+      })
+      .then((ret) => {
+          // creates a random songlist to give variety to home page lists
+          const randSongList = shuffle(ret)
+        setRandomSongList(randSongList)
+        // setPicksList(randSongList.slice(songList.length - 8, songList.length))
+      })
+      .then((randSongList) => {
+      })
+      .then(setLoaded(true))
+  },[]);
+
+  useEffect(() => {
+    dispatch(getSongs())
+      .then((ret) => {
+        setSongList(ret)
+      })
+      .then(setLoaded(true))
+  },[hearted]);
+
+
+
+
 
   let popSongList = songList.map(song => song).sort((songA, songB) => songB.Hearts.length - songA.Hearts.length);
 
