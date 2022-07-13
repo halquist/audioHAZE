a532eced07e69fa34ac1b1bc28514449bb52f79f
jon <jon.halquist@gmail.com>
2022-07-13 14:04:39 -0500

    added playlist model and migration

---
+++ backend/db/migrations/20220713184351-create-playlist.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Playlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(100)
+      },
+      playlist: {
+        type: Sequelize.ARRAY,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Playlists');
+  }
+};
---
+++ backend/db/models/playlist.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Playlist = sequelize.define('Playlist', {
+    userId: {
+      allowNull: false,
+      type:  DataTypes.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING(100),
+    },
+    playlist: {
+      type: DataTypes.ARRAY,
+      defaultValue: [],
+    },
+
+  }, {});
+
+  Playlist.create = async function ({ userId, title }) {
+    const playlist = await Playlist.create({
+      userId,
+      title
+    });
+    return await Playlist.findByPk(playlist.id);
+  }
+
+  Playlist.delete = async function ({ id }) {
+    const playlistFind = await Playlist.findByPk(id)
+    await playlistFind.destroy();
+    return {
+      message: 'Success'
+    }
+  }
+
+  Playlist.add = async function ({ playlistId, songId }) {
+    const playlistFind = await Playlist.findByPk(playlistId);
+    // const playlistArr = playlistFind.playlist;
+    playlistFind.playlist.push(songId);
+    await playlistFind.save();
+    return playlistFind;
+  }
+
+  Playlist.remove = async function ({ playlistId, songId }) {
+    const playlistFind = await Playlist.findByPk(playlistId);
+    // const playlistArr = playlistFind.playlist;
+    const removeIdx = playlistFind.playlist.indexOf(songId);
+    playlistFind.playlist.splice(removeIdx, 1);
+    await playlistFind.save();
+    return playlistFind;
+  }
+
+  Playlist.associate = function(models) {
+    // associations can be defined here
+    Playlist.belongsTo(models.User, { foreignKey: 'userId'})
+  };
+  return Playlist;
+};
